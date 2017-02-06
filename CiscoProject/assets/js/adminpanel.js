3acc440f2beff61681e52dd74d8abfc8b249bf6d
Nireekshitha <nireekshithachennavara@gmail.com>
2017-02-06 18:01:32 +0530

    Add files via upload

---
+++ CiscoProject/assets/js/adminpanel.js
@@ -0,0 +1,9 @@
+$('#event').select2({
+    tags: ["event1", "event2"],
+     maximumSelectionSize:1
+});
+$('#subEvent').select2({ tags: ["subevent1", "subevent2"],
+ maximumSelectionSize:1
+});
+  $('#category').select2({ tags: ["category1", "category2"],
+ maximumSelectionSize:1 });
